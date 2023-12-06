a5bf8482c3bc22d6136eaeaacf35a09e6621637b
Tomasz Szczygiel <szczgl@wp.pl>
2023-12-06 11:25:45 +0100

    6.2 Added kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,24 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
-        // zadanie dodatkowe
-        TestingMain testingMain = new TestingMain();
-        testingMain.vowelSearchInAString("Alabama jest mIastem");
 
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-    }
-    public void vowelSearchInAString(String string) {
-        String[] stringToCompare = new String[] {"e","a","y","u","i","o","E","A","Y","U","I","O"} ;
-        String[] splitEnterString =  string.split("");
-        for (int i = 0; i < string.length(); i++) {
-            for (int j = 0; j < stringToCompare.length; j++) {
-                if(stringToCompare[j].equals(splitEnterString[i])) {
-                    String word = splitEnterString[i];
-                    //System.out.print(word);
-                }
-            }
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calculator = new Calculator(5,4);
+        int addResoult = calculator.add();
+        int substractResoult = calculator.substract();
+
+        if (addResoult == 9) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        if (substractResoult == 1) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
         }
     }
 }
